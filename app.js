0fbcf665dbe1ee57a86f83e7d9d2833a7ab8b162
Victor López <vic@Victors-MacBook-Pro.local>
2020-05-27 14:24:51 -0500

    module 2

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-	res.send('<h1>I love Treehouse!</h1>');
+	res.render('index');
 });
 
-app.get('/hello', (req, res) => {
-	res.send('<h1>Hello, JavaScript Developer!</h1>');
+app.get('/cards', (req, res) => {
+	res.render('card', {prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is."});
 });
 
 app.listen(3000, () => {
